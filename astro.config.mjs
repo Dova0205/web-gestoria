8c68c65328914240ca56247396d4662ec9b75b7b
Dova0205 <92132138+Dova0205@users.noreply.github.com>
2026-01-12 16:47:21 -0300

    Corregir configuracion Astro y nombre de archivo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://Dova0205.github.io', // Tu usuario de GitHub
+  base: '/web-gestoria',             // El nombre de tu repositorio
+});
